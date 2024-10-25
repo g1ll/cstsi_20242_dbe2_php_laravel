6ee0850c1838d1ddabaf3563fb8b4ba5c31281a0
g1ll <gillvelleda@gmail.com>
2024-10-25 18:24:52 -0500

    feat: exemplo facede View::make()

--- aula03/app/Http/Controllers/HomeController.php
+++ aula03/app/Http/Controllers/HomeController.php
@@ -3,12 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\View;
 
 class HomeController extends Controller
 {
     public function index() {
         $text = "Primeiro exemplo de controller!!!";
-        return view('home',[
+        // dump($text);// var_dump(); die;//Helpers
+        // dd($this);
+        // return view('home',[
+        //     "content"=>$text
+        // ]);
+
+        return View::make('home',[
             "content"=>$text
         ]);
     }
