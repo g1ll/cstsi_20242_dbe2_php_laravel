fdc802fca0aa933ce5e43433dde6b4cf25809f3c
g1ll <gillvelleda@gmail.com>
2024-11-08 18:59:49 -0500

    feat: criacao do recurso de api Protudo com rotas e ResourceCollection

--- aula03/app/Http/Controllers/Api/ProdutoController.php
+++ aula03/app/Http/Controllers/Api/ProdutoController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\ProdutoCollection;
 use App\Models\Produto;
 use Illuminate\Http\Request;
 
@@ -13,7 +14,7 @@ class ProdutoController extends Controller
      */
     public function index()
     {
-        //
+        return new ProdutoCollection(Produto::all());
     }
 
     /**
--- aula03/routes/api.php
+++ aula03/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\ProdutoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -7,3 +8,5 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+
+Route::apiResource('produtos',ProdutoController::class);
