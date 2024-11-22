ba7b0b8a1e57c3a5eadc7a51ff1574cb01f80468
g1ll <gillvelleda@gmail.com>
2024-11-22 18:37:16 -0500

    feat: criacao de middleware para forçar resposta json da api.

--- aula03/bootstrap/app.php
+++ aula03/bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\ForceJsonResponse;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -12,7 +13,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        // $middleware->prependToGroup('api',ForceJsonResponse::class);
+        $middleware->api(prepend: ForceJsonResponse::class);
+        // dd($middleware->getMiddlewareGroups());
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
