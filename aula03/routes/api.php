fc0b3bc9b6f4dace8884da51057dc9e52e767f86
g1ll <gillvelleda@gmail.com>
2024-11-22 18:39:55 -0500

    feat: proteção de rotas da api com o middleware sanctum.

--- aula03/routes/api.php
+++ aula03/routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\Api\LoginController;
 use App\Http\Controllers\Api\ProdutoController;
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -8,4 +10,19 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::apiResource('produtos',ProdutoController::class);
+
+
+Route::apiResource('produtos',ProdutoController::class)
+        ->middleware('auth:sanctum');//todas protegidas (post, put, delete)
+
+Route::apiResource('produtos',ProdutoController::class)
+        ->only(['index','show']);//sobreescreve a proteção de index e show
+
+
+Route::apiResource('users',UserController::class);//Desprotegidas
+Route::apiResource('users',UserController::class)
+    ->middleware('auth:sanctum')
+    ->except(['store']);//cadastro
+
+
+Route::post('/login', [LoginController::class, 'login']);
