234d6412339cd1cf2167930bf40a2caacfc15e47
g1ll <gillvelleda@gmail.com>
2024-11-29 20:51:32 -0500

    feat: criação da factory de fornecedore e atualização da model Fornecedor.

--- aula03/app/Models/Fornecedor.php
+++ aula03/app/Models/Fornecedor.php
@@ -2,12 +2,29 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Fornecedor extends Model
 {
+
+    use HasFactory;
+
+    protected $fillable = [
+        "nome",
+        "cnpj",
+        "email",
+        "estado_id",
+        "telefone",
+        "endereco"
+    ];
+
     protected $table='fornecedores';
 
+    public function estado()
+    {
+        return $this->belongsTo(Estado::class);
+    }
 
     public function produtos()
     {
