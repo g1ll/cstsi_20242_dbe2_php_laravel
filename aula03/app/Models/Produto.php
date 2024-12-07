01153494adb4b0481f604900158cf6e0d154f4bc
g1ll <gillvelleda@gmail.com>
2024-12-06 19:53:03 -0500

    feat: cria o relacionamento de Produto com Promocoes e adiciona Query Scopes.

--- aula03/app/Models/Produto.php
+++ aula03/app/Models/Produto.php
@@ -31,33 +31,41 @@ class Produto extends Model
         return $this->belongsTo(Fornecedor::class);
     }
 
+    public function promocoes()
+    {
+        return $this->belongsToMany(Promocao::class)
+            ->withPivot('desconto')
+            ->withTimestamps();
+    }
+
+
     //https://laravel.com/docs/11.x/eloquent#query-scopes
-    // public function scopeImportados($query)
-    // {
-    //     return $query->where('importado', true);
-    // }
+    public function scopeImportados(Builder $query)
+    {
+        return $query->where('importado', true);
+    }
 
     //query scope Local
-    // public function scopePorRegiao(Builder $query, string $nome)
-    // {
-    //     return $query->whereHas(
-    //         'fornecedor',
-    //         fn($q) => $q->whereHas(
-    //             'estado',
-    //             fn($q) => $q->whereHas(
-    //                 'regiao',
-    //                 fn($q) => $q->where('nome', 'like', $nome)
-    //             )
-    //         )
-    //     );
-    // }
+    public function scopePorRegiao(Builder $query, string $nome)
+    {
+        return $query->whereHas(
+            'fornecedor',
+            fn($q) => $q->whereHas(
+                'estado',
+                fn($q) => $q->whereHas(
+                    'regiao',
+                    fn($q) => $q->where('nome', 'like', $nome)
+                )
+            )
+        );
+    }
 
 
     //escopo global
-    // protected static function booted()
-    // {
-    //     static::addGlobalScope('fornecedor', function (Builder $builder) {
-    //         $builder->with('fornecedor');
-    //     });
-    // }
+    protected static function booted()
+    {
+        static::addGlobalScope('fornecedor', function (Builder $builder) {
+            $builder->with('fornecedor');
+        });
+    }
 }
